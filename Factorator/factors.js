b2b91432a50d4f2afcaaf397cbe66cbdbe01f408
Jan Kooijman <jcz.kooijman@hetnet.nl>
2024-08-12 08:58:44 +0200

    Hazelaar: zet naam file in meervoud

--- Factorator/factors.js
+++ Factorator/factors.js
@@ -1,23 +1,23 @@
-document.getElementById('calculateButton').onclick = arrangeThings;
+document.getElementById('calculateButton').onclick = arrange;
 
-function arrangeThings() { 
+function arrange() { 
    const numberInput = document.getElementById('numberInput').value;    
    const resultDiv = document.getElementById('result');    
    if (numberInput === '' || isNaN(numberInput)) {        
-   resultDiv.innerHTML = 'Please enter a valid number.';        
-   return;    
+	   resultDiv.innerHTML = 'Please enter a valid number.';        
+	   return;    
    }        
    const number = parseInt(numberInput);    
    const factors = calculateFactors(number);    
-   // resultDiv.innerHTML = numberInput;
    resultDiv.innerHTML = `Factors of ${number}: ${factors.join(', ')}`;
-   }; 
+};
    
 function calculateFactors(number) {
     const factors = [];    
     for (let i = 1; i <= number; i++) {        
 	    if (number % i === 0) {            
-	    factors.push(i);        
+	    	factors.push(i);        
 	    }    
     }    
-    return factors; }
+    return factors; 
+}
